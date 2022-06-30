1ee91de57bfd8ae22cce242ff6ae42778f7058b2
Stanislau <stas1408123@gmail.com>
2022-06-30 05:36:25 +0300

    Addded example of api and unit test

---
+++ ExampleCICD/ExampleAPI/Controllers/NumberController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExampleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumberController : ControllerBase
+    {
+
+        private readonly ILogger<NumberController> _logger;
+
+        public NumberController(ILogger<NumberController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int GetFive()
+        {
+            return 5;
+        }
+
+    }
+}
---
+++ ExampleCICD/ExampleApi.Test/NumberControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using ExampleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExampleApi.Test
+{
+    public class NumberControllerTests
+    {
+
+        private readonly Mock<ILogger<NumberController>> _logger = new();
+
+
+        [Fact]
+        public async Task GetFive_ShouldReturnValidModel()
+        {
+            // Arrange
+            int data = 5;
+
+            var controller = new NumberController(_logger.Object);
+
+            // Act
+            var result = controller.GetFive();
+
+            // Assert
+            data.ShouldBeEquivalentTo(result);
+        }
+    }
+}
